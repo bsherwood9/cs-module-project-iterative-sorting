426f6bc6b22dd5268319553e5826b82de1c542f2
bsherwood9 <36565536+bsherwood9@users.noreply.github.com>
2020-09-30 22:11:28 -0600

    Met mvp. Need to do stretch.

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,16 +1,19 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
+    for i in range(0, len(arr)):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
+        for j in range(cur_index+1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
         # TO-DO: swap
         # Your code here
+        arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
 
     return arr
 
@@ -18,9 +21,22 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
-
+    # adding the swaps to tell us when it is mostly sorted.
+    swaps = True
+    for f in range(len(arr)-1):
+        swaps = True
+        print(f)
+        for i in range(len(arr)-f-1):
+            print("length", len(arr)-f-1)
+            if arr[i] > arr[i+1]:
+                temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+                swaps = False
+        if swaps:
+            break
     return arr
+
 
 '''
 STRETCH: implement the Counting Sort function below
@@ -31,16 +47,30 @@ data. The idea behind this algorithm then is that we can create "buckets"
 from 0 up to the max value. This is most easily done by initializing an
 array of 0s whose length is the max value + 1 (why do we need this "+ 1"?).
 
-Each buckets[i] then is responsible for keeping track of how many times 
+Each buckets[i] then is responsible for keeping track of how many times
 we've seen `i` in the input set of data as we iterate through it.
 Once we know exactly how many times each piece of data in the input set
-showed up, we can construct a sorted set of the input data from the 
-buckets. 
+showed up, we can construct a sorted set of the input data from the
+buckets.
 
 What is the time and space complexity of the counting sort algorithm?
 '''
+
+
 def counting_sort(arr, maximum=None):
     # Your code here
+    buckets = [0 for i in range(0, maximum+1)]
+    for i in arr:
+        for j in buckets:
+            pass
+    return arr
 
 
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        val = arr[i]
+        j = i
+        while j > 0 and val < arr[j-1]:
+            arr[j], arr[j-1] = arr[j-1], arr[j]
+            j -= 1
     return arr
